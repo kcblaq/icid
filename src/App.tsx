81f05a2ab526d3e2a8ba9e7d774dd9285f8bc42d
kcblack22@gmail.com <kcblack22@gmail.com>
2023-02-03 00:58:54 +0100

    More text detail

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ function App() {
   return (
     <div className="App">
      <h3> Just me testing CICD </h3>
+
+     <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam, architecto maiores itaque soluta sequi dignissimos fuga! Adipisci ad consequuntur et sint libero earum vero, optio consequatur officia impedit, quae eveniet.</p>
     </div>
   )
 }
